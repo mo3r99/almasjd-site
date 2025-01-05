0acad0252bd657e2184a713ccd526e715d652c41
Muhammad Rauf <muhammadrauf@DeviceBook.local>
2025-01-05 20:42:22 +0000

    loading

---
+++ app/loading.js
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center w-full h-screen">
+      <div className="w-10 h-10 border-2 border-t-0 border-weborange rounded-full animate-spin"></div>
+    </div>
+  );
+}
