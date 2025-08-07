2eff6ceeb058baa1db63a90a3df479ff6bcec964
Muhammad Rauf <mo3r99@gmail.com>
2025-08-07 18:52:28 +0100

    update heading

--- app/ui/heading/heading.js
+++ app/ui/heading/heading.js
@@ -1,6 +1,6 @@
-export default function Heading({children, ...props}) {
+export default function Heading({children, id, ...props}) {
     const style = `text-4xl font-[family-name:var(--font-raleway)] font-semibold ${props.className && props.className}`
     return(
-        <h1 className={style} {...props}>{children}</h1>
+        <h1 className={style} id={id}>{children}</h1>
     )
 }
