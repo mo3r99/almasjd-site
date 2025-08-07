282df1dab66b68d138ff75d7cbcbbe4d4e6875b1
Muhammad Rauf <mo3r99@gmail.com>
2025-08-07 18:30:35 +0100

    fix heading

--- app/ui/heading/heading.js
+++ app/ui/heading/heading.js
@@ -1,6 +1,6 @@
 export default function Heading({children, ...props}) {
     const style = `text-4xl font-[family-name:var(--font-raleway)] font-semibold ${props.className && props.className}`
     return(
-        <h1 className={style}>{children}</h1>
+        <h1 className={style} {...props}>{children}</h1>
     )
 }
