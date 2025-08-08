4203e18cc8a7a88ea6abb01d985803b89934b407
Muhammad Rauf <mo3r99@gmail.com>
2025-08-08 11:01:43 +0100

    change allowed users

--- auth.js
+++ auth.js
@@ -1,13 +1,7 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
-const allowedUsers = [
-  "m.rauf@almasjid.co.uk",
-  "hamza.malik@almasjid.co.uk",
-  "ibrahim.rauf@almasjid.co.uk",
-  "hamza.rauf@almasjid.co.uk",
-  "mo3r99@gmail.com"
-];
+const allowedUsers = process.env.ALLOWED_USERS.split(' ');
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   debug: true,
