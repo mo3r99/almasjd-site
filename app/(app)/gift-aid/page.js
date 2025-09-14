655549cd0e4959e06b95964f1b61fae5daa5d6db
M <mo3r99@gmail.com>
2025-09-14 12:41:35 +0100

    Update redirect URL in GiftAidPage component

--- app/(app)/gift-aid/page.js
+++ app/(app)/gift-aid/page.js
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation"
 
 export default function GiftAidPage() {
-    return redirect('https://forms.office.com/Pages/ResponsePage.aspx?id=ZOrjoRGHW0a27251DWrQb7SjwJ0Q6f1Dm73UP8eB77pUOFFWSEJCVUZONEU4MlVIWDlCMlpKTzRCUy4u')
+    return redirect('https://forms.office.com/Pages/ResponsePage.aspx?id=Q1sH_SUfqkaLxul0jq9syeD8k54sSshHudLQ7gjCZlRUQkVENEtSWDlWM09IOENDQVJPUlVQMEI4SS4u')
 }
