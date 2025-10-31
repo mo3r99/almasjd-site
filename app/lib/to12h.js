d6da7d879c4bbc7899d0c13d4662e01e9e34b96d
Muhammad Rauf <muhammad.s.rauf@proton.me>
2025-10-31 17:40:58 +0000

    fix 12h function

--- app/lib/to12h.js
+++ app/lib/to12h.js
@@ -1,11 +1,15 @@
 export default function to12h(time) {
-    let [h,m] = time.split(':').map(Number);
-    let suffix = 'am';
+  let [h, m] = time.split(":").map(Number);
+  let suffix = "am";
 
-    if (h > 12) {
-        h = h - 12;
-        suffix = 'pm';
-    }
+  if (h >= 12) {
+    h = h - 12;
+    suffix = "pm";
+  }
 
-    return `${h}:${m < 10 ? '0' + m : m } ${suffix}`;
+  if (h == 0) {
+    h = 12;
+  }
+
+  return `${h}:${m < 10 ? "0" + m : m} ${suffix}`;
 }
