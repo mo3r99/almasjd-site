9744d2e13a337fcfad5cd0375442d0484d196504
M <muhammad.s.rauf@proton.me>
2026-01-22 12:47:58 +0000

    Update redirect URL in GiftAidPage

--- app/(app)/gift-aid/page.js
+++ app/(app)/gift-aid/page.js
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation"
 
 export default function GiftAidPage() {
-    return redirect('https://forms.office.com/Pages/ResponsePage.aspx?id=Q1sH_SUfqkaLxul0jq9syeD8k54sSshHudLQ7gjCZlRUQkVENEtSWDlWM09IOENDQVJPUlVQMEI4SS4u')
+    return redirect('https://forms.microsoft.com/e/8CZLLtmkSp')
 }
