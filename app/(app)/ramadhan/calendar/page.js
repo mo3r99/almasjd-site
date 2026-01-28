e075d792fa08367c1b886ab80fc23a59d5a87e96
M <muhammad.s.rauf@proton.me>
2026-01-28 14:27:43 +0000

    Update Ramadhan calendar links and images for 2026

--- app/(app)/ramadhan/calendar/page.js
+++ app/(app)/ramadhan/calendar/page.js
@@ -10,7 +10,7 @@ export default function Calendar() {
   return (
     <>
       <a
-        href="https://almasjid-site.s3.eu-north-1.amazonaws.com/ramadhan-calendar-2025-print-friendly.pdf"
+        href="https://almasjid-site.s3.eu-north-1.amazonaws.com/Ramadhan+2026+Timetable.pdf"
         download="Ramadhan Calendar"
         className="mx-auto my-4 p-4 bg-tradewind text-white rounded-md hover:bg-[#559c8c] flex gap-2"
       >
@@ -22,7 +22,7 @@ export default function Calendar() {
       <Image
         width={800}
         height={1200}
-        src={"//wsrv.nl/?url=almasjid-site.s3.eu-north-1.amazonaws.com/calendar.jpeg&w=1200&h=auto"}
+        src={"//wsrv.nl/?url=almasjid-site.s3.eu-north-1.amazonaws.com/Ramadhan+2026.png&w=1200&h=auto"}
         unoptimized
         className="mx-auto hidden md:block my-4"
         alt='Ramadhan Calendar'
@@ -31,7 +31,7 @@ export default function Calendar() {
       <Image
         width={500}
         height={900}
-        src={"//wsrv.nl/?url=almasjid-site.s3.eu-north-1.amazonaws.com/calendar.jpeg&w=900&h=auto"}
+        src={"//wsrv.nl/?url=almasjid-site.s3.eu-north-1.amazonaws.com/Ramadhan+2026.png&w=900&h=auto"}
         unoptimized
         className="mx-auto block md:hidden my-4"
         alt='Ramadhan Calendar'
